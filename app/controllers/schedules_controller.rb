a82f5299ee2cf2f633d0647cc7c9da0273c799a8
joni-deppu <44shuxichuan@gmail.com>
2022-09-10 02:44:52 +0900

    トップページから予約

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,25 @@
 class SchedulesController < ApplicationController
 
   def index
-    
+    @schedule = Schedule.all
   end
+
+  def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:scheduled_date, :time_zone_id, :team_id).merge(user_id: current_user.id)
+  end  
 end
